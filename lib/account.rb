dd32796f7766472ffef9350c8a7cf85cb9f2ddba
Ralph Mallett <rmallett12@gmail.com>
2020-09-07 15:01:22 +0100

    Add method and tests for displaying transactions

--- lib/account.rb
+++ lib/account.rb
@@ -1,16 +1,23 @@
 class Account
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
   def display_balance
     format '%.2f', @balance
   end
 
-  def deposit(amount)
+  def display_transactions
+    @transactions.join
+  end
+
+  def deposit(amount, date)
     @balance += amount
+    formatted_amount = format '%.2f', amount
+    @transactions.push(date, ' || ', formatted_amount, ' || || ', display_balance)
   end
 
   def withdraw(amount)
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,24 +8,30 @@ describe Account do
   end
   describe '.deposit' do
     it 'changes the balance by the amount deposited' do
-      subject.deposit(1000)
+      subject.deposit(1000, '10/01/2012')
       expect(subject.display_balance).to eq('1000.00')
     end
     it 'changes can handle decimal deposits' do
-      subject.deposit(1000.50)
+      subject.deposit(1000.50, '10/01/2012')
       expect(subject.display_balance).to eq('1000.50')
     end
   end
   describe '.withdraw' do
     it 'changes the balance by the amount withdrawn' do
-      subject.deposit(1000)
+      subject.deposit(1000, '10/01/2012')
       subject.withdraw(500)
       expect(subject.display_balance).to eq('500.00')
     end
     it 'changes can handle decimal deposits' do
-      subject.deposit(1000.50)
+      subject.deposit(1000.50, '10/01/2012')
       subject.withdraw(500)
       expect(subject.display_balance).to eq('500.50')
+    end
+  end
+  describe '.display_transactions' do
+    it 'displays transaction details and date for a deposit' do
+      subject.deposit(1000, '10/01/2012')
+      expect(subject.display_transactions).to eq('10/01/2012 || 1000.00 || || 1000.00')
     end
   end
 end
